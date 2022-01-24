2bce43b4c9e82faf2ed14c9b204a9d14df954398
HemaBabyDoge <ebrahim@babydogecoin.com>
2022-01-24 10:59:02 -0500

    Added Deployment Script

--- hardhat.config.js
+++ hardhat.config.js
@@ -84,6 +84,12 @@ module.exports = {
       accounts:
           process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: true !== undefined,
